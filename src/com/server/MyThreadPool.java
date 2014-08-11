5ea1fcf6f4d4397a610e3da11e6cb50dc704e0bb
jackarrow <jackrunsheng@gmail.com>
2014-08-11 10:45:33 +0800

    first commit

--- src/com/server/MyThreadPool.java
+++ src/com/server/MyThreadPool.java
@@ -1,7 +1,9 @@
 package com.server;
 
+import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 public class MyThreadPool {
 	private static MyThreadPool pool  = new MyThreadPool();
@@ -18,4 +20,11 @@ public class MyThreadPool {
 		service.execute(task);
 	}
 	
+	public void shutDownThread() throws InterruptedException{
+		service.awaitTermination(5, TimeUnit.SECONDS);
+		List<Runnable> tasks = service.shutdownNow();//执行任务后的剩下的未执行的任务
+		if(tasks.size()==0){
+			//当前没有执行中的任务
+		}
+	}
 }
